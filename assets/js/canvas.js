dcd125428b2922d0e94a5def90e6d54ef8b17105
Sydorenko Danylo <sydorenkodanylo2021@gmail.com>
2021-10-21 02:08:30 +0300

    yes

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -22,12 +22,18 @@ function canvas(selector, options) {
             dragging: dragging
         })
     }
-
+    const img = new Image;
+    img.src =`assets/img/backg.jpg`;
+    img.onload = () => {
+        context.drawImage(img, 0, 0);
+    }
+    context.drawImage(img, 0, 0);
     // головна функція для малювання
     const redraw = () => {
         //очищуємо  canvas
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 
+
+        // context.clearRect(0, 0, context.canvas.width, context.canvas.height);
         context.strokeStyle = options.strokeColor;
         context.lineJoin = "round";
         context.lineWidth = options.strokeWidth;
@@ -44,6 +50,7 @@ function canvas(selector, options) {
             context.stroke();
             prevPoint = point;
         }
+        // context.beginPath();
     }
 
     // функції обробники подій миші
@@ -53,7 +60,7 @@ function canvas(selector, options) {
         redraw();
     }
 
-
+// dragging = перетягування
     const mouseMove = event => {
         if(isPaint){
             addPoint(event.pageX, event.pageY, true);
@@ -82,12 +89,15 @@ function canvas(selector, options) {
 // тут необхідно додати код очистки canvas та масиву точок (clearRect)
         context.clearRect(0, 0, context.canvas.width, context.canvas.height);
         points = [];
+        context.drawImage(img, 0, 0);
+        // context.beginPath();
+        // context.closePath();
 //         redraw();
     })
 
 
     /*           Download button               */
-    // doesn't work #TODO do it working element
+    // opens new page with only .png image
     const downloadBtn = document.createElement('button');
     downloadBtn.classList.add('btn');
     downloadBtn.textContent = 'Download';
@@ -122,12 +132,63 @@ function canvas(selector, options) {
         console.log(points);
     })
 
+    const timeBtn = document.createElement('button');
+
+    timeBtn.classList.add('btn');
+    timeBtn.textContent = 'Timestamp';
+    timeBtn.addEventListener('click', () => {
+        currentData = new Date();
+        context.strokeStyle = '';
+        context.beginPath();
+        context.font = '15px serif';
+        context.strokeText(currentData.toString(), 10, 50, rect.width);
+        // context.fill();
+        context.closePath();
+        context.stroke();
+        // redraw();
+    })
+
+    let color;
+    const colorBtn = document.createElement('btn');
+    colorBtn.classList.add('btn');
+    colorBtn.textContent = 'Change color';
+    colorBtn.innerHTML = '<img src="assets/img/floppy-disk.png" height="30px" width="30px" />';
+    colorBtn.addEventListener('click', () => {
+        color = document.getElementById("color-picker");
+        console.log(color);
+        console.log(color.value);
+
+
+        // const scannedImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        // const scannedData = scannedImage.data;
+        // options.strokeColor = color.value;
+        // ctx.putImageData(scannedImage, 0, 0);
+        // redraw();
+        options.strokeColor = color.value;
+        // context.strokeStyle = color.value;
+        // redraw();
+        // context.beginPath();
+
+        // options.strokeStyle = color.value;
+        // points1 = points;
+        // options.strokeWidth = 1;
+        // canvas(selector, options)
+
+        // context.closePath();
+    })
+
+    toolBar.insertAdjacentElement('afterbegin', colorBtn);
+    toolBar.insertAdjacentElement('afterbegin', timeBtn);
+    // document.getElementById('toolbar').appendChild(colorBtn)
     toolBar.insertAdjacentElement('afterbegin', saveBtn);
     toolBar.insertAdjacentElement('afterbegin', restoreBtn);
     toolBar.insertAdjacentElement('afterbegin', downloadBtn);
-    toolBar.insertAdjacentElement('afterbegin', clearBtn)
+    toolBar.insertAdjacentElement('afterbegin', clearBtn);
+
+    // TODO 4.5 | криво але зроблено
 
 
+    // background
 
 
 }
