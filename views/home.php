ede56ca2a1d3fcfb0e502789ef47ec848cfdc12c
Sydorenko Danylo <sydorenkodanylo2021@gmail.com>
2021-12-03 15:45:25 +0200

    changes

--- views/home.php
+++ views/home.php
@@ -17,64 +17,13 @@ if (isset($_SESSION['auth']) && $_SESSION['auth'] === true) {
 <!--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">-->
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/bootstrap-5.0.2-dist/css/bootstrap.css">
-    <style>
-        body{
-            padding-top: 3rem;
-        }
-        .container {
-            width: 400px;
-        }
-    </style>
 </head>
 <body>
 <script src="../assets/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-            </ul>
-            <div class="navbar-nav ms-5">
-                <ul class="navbar-nav mx-3-0">
-                    <a class="nav-link" href="#">My Text</a>
-                </ul>
-            </div>
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-        </div>
-    </div>
-</nav>
+<?php require "navbar.php"?>
 
-<?php if($isRestricted):?>
+<?php //if($isRestricted):?>
     <form action="?controller&action=logout" method="post" enctype="multipart/form-data">
         <input type="submit" class="btn right" value="Logout">
     </form>
@@ -86,7 +35,7 @@ if (isset($_SESSION['auth']) && $_SESSION['auth'] === true) {
             <a class="btn" href="?controller=roles">List of all Roles</a>
         </div>
     </div>
-<?php else:?>
+<?php //else:?>
 <div class="container">
     <h3>Control Panel</h3>
     <form action="?controller&action=auth" method="post">
@@ -102,7 +51,7 @@ if (isset($_SESSION['auth']) && $_SESSION['auth'] === true) {
         </div>
         <input type="submit" class="btn" value="Login">
     </form>
-    <?php endif;?>
+<!--    --><?php //endif;?>
 </div>
 </body>
 </html>
--- views/navbar.php
+++ views/navbar.php
@@ -1,52 +1,36 @@
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+<div class="container-fluid">
+<!--    <a class="navbar-brand" href="#">Navbar</a>-->
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown
-          </a>
-        </li>
-      </ul>
-      <form class="d-flex align-content-center">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-        <ul class="navbar-nav me-0">
-            <div class="row">
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Sign In</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Sign In</a>
-                </li>
-            </div>
+        <ul class="navbar-nav mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
         </ul>
-
+        <form class="d-flex mx-auto">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        <ul class="navbar-nav mx-3">
+            <li class="nav-item">
+                 <?php if (isset($isRestricted) && $isRestricted):?>
+                     <a class="nav-link active" aria-current="page" href="#">Hello, <?php echo $_SESSION['username'] ?></a>
+                 <?php else:?>
+                     <a class="nav-link active" aria-current="page" href="#">Sign In</a>
+                 <?php endif;?>
+            </li>
+            <div class="vl"></div>
+            <li class="nav-item">
+                <?php if (isset($isRestricted) && $isRestricted):?>
+                    <a class="nav-link active" aria-current="page" href="#">Sign out</a>
+                <?php else:?>
+                    <a class="nav-link active" aria-current="page" href="#">Sign up</a>
+                <?php endif;?>
+            </li>
+        </ul>
     </div>
-  </div>
-</nav>
-
-
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-    <div class="hstack gap-3">
-        <input class="form-control me-auto" type="text" placeholder="Add your item here..." aria-label="Add your item here...">
-        <button type="button" class="btn btn-secondary">Submit</button>
-        <div class="vr"></div>
-        <button type="button" class="btn btn-outline-danger">Reset</button>
-    </div>
-    </div>
+</div>
 </nav>
